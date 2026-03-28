9886334a2fc6ebc048b9278103d21a2c0518e8a3
pvraoveera <pvraoveeraster92@gmail.com>
2026-03-28 15:56:54 +0530

    docker

--- SPLUNKENTERPRISE/entrypoint.sh
+++ SPLUNKENTERPRISE/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-${SPLUNK_HOME}/bin/splunk stop
-${SPLUNK_HOME}/bin/splunk disable boot-start
-${SPLUNK_HOME}/bin/splunk enable boot-start -systemd-managed 1 -user splunk -group splunk
+${SPLUNK_HOME}/bin/splunk enable boot-start -systemd-managed 1
 ${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --no-prompt
 # Keep container alive
 tail -f /opt/splunk/var/log/splunk/splunkd.log
